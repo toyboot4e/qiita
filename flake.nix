b0f8c381db1278028db186e9496b4440480fa448
toyboot4e <toyboot4e@gmail.com>
2025-11-23 04:02:45 +0900

    new: add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+          ];
+
+          shellHook = ''
+            npm install -D @qiita/qiita-cli
+          '';
+        };
+      });
+}
